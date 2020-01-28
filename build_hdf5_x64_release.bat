220016759a896ea7f84473c84317dd92cc579cf3
Michael Huth <Michael.Huth@byte-physics.de>
2020-01-28 17:48:31 +0100

    gitlab CI script for build + test

--- build_hdf5_x64_release.bat
+++ build_hdf5_x64_release.bat
@@ -4,4 +4,5 @@ cd hdf5_package
 7z x -y CMake-hdf5-1.10.6.zip *
 cd CMake-hdf5-1.10.6
 ctest -S HDF5config.cmake,BUILD_GENERATOR=VS201964 -C Release -V -O hdf5.log -parallel 16
-7z x -y -o..\..\hdf5 HDF5-1.10.6-win64.zip *
+move /Y HDF5-1.10.6-win64.zip hdf5_x64_release.zip
+7z x -y -o..\..\hdf5 hdf5_x64_release.zip *
--- build_x64_debug.bat
+++ build_x64_debug.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /D %~dp0
+cd hdf5_package\CMake-hdf5-1.10.6
+7z x -y -o..\..\hdf5 hdf5_x64_debug.zip *
+
+cd /D %~dp0
 md build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 16 2019" -A x64 ..\src
